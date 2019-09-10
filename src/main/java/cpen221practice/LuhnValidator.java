a303064792bdb14c128a84eb01927f8856fa1916
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2019-09-09 23:49:09 -0700

    init. repo

---
+++ src/main/java/cpen221practice/LuhnValidator.java
@@ -0,0 +1,9 @@
+package cpen221practice;
+
+class LuhnValidator {
+
+    boolean isValid(String candidate) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
